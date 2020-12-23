1adfffe75f0a08814db62d67abf31d6b206df148
ustdn07 <ustdn07@iiht.tech>
2020-12-23 19:52:24 +0530

    Add project files.

---
+++ SpecFlowSample/Feature/SpecFlowFeature.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeatureLoginVerification
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+#@mytag
+#Scenario: Add two numbers
+#	Given the first number is 50
+#	And the second number is 70
+#	When the two numbers are added
+#	Then the result should be 120
+
+@LoginTest
+Scenario Outline: Successfull Login
+   Given I have the correct username <username> and password <password>
+   When Iclick the submit button
+   Then I will be taken to Home page
+   Examples: 
+   | username | password |
+   | unni    |   unni78 |
---
+++ SpecFlowSample/StepDefinition/SpecFlowFeatureLoginVerificationSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSample.StepDefinition
+{
+    [Binding]
+    public class SpecFlowFeatureLoginVerificationSteps
+    {
+        [Given(@"I have the correct username unni and password unni(.*)")]
+        public void GivenIHaveTheCorrectUsernameUnniAndPasswordUnni(int p0)
+        {
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Iclick the submit button")]
+        public void WhenIclickTheSubmitButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I will be taken to Home page")]
+        public void ThenIWillBeTakenToHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
